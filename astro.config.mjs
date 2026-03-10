b17a25484a748e263b847cbf54e2373fbc1233d0
Rahim <forbdruid@gmail.com>
2026-03-10 16:55:07 +0500

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://happyhaha.github.io',
+  base: '/solar-series',
   vite: {
     plugins: [tailwindcss()]
   }
